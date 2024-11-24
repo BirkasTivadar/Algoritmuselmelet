f952d6429178b8a0c39ef32ed7ccb8e7ae24d6c2
BirkasTivadar <birkastivadar@gmail.com>
2024-11-24 11:16:02 +0100

    Beszúrásos rendezés

---
+++ Rendezes/beszurasos.py
@@ -0,0 +1,26 @@
+import random
+import time
+
+def beszurasos(A):
+    for j in range(1, len(A)):
+        kulcs = A[j]
+        i = j - 1
+        while i >= 0 and A[i] > kulcs:
+            A[i + 1] = A[i]
+            i -= 1
+
+        A[i + 1] = kulcs
+
+
+A = list(range(10000))
+
+random.shuffle(A)
+
+print(A)
+start = time.time()
+beszurasos(A)
+end = time.time()
+
+print(A)
+print(end-start)
+
