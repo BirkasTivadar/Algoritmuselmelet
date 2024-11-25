a5ae2dec2ede4758da3e4389cce6d59e8937aace
BirkasTivadar <birkastivadar@gmail.com>
2024-11-25 21:23:18 +0100

    Gyorsrendezés

---
+++ Rendezes/gyorsrendezes.py
@@ -0,0 +1,27 @@
+from teszteles import tesztelo
+
+
+def feloszt(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+
+    A[i + 1], A[r] = A[r], A[i + 1]
+    return i + 1
+
+
+def gyorsrendezes(A, p, r):
+    if p < r:
+        q = feloszt(A, p, r)
+        gyorsrendezes(A, p, q - 1)
+        gyorsrendezes(A, q + 1, r)
+
+
+def gyorsrendezo(A):
+    gyorsrendezes(A, 0, len(A) - 1)
+
+
+# tesztelo(gyorsrendezo, 10000)
