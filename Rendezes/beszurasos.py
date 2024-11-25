f83fefa0b95e6dea07fe80722f4f0df9913f3547
BirkasTivadar <birkastivadar@gmail.com>
2024-11-25 20:41:25 +0100

    Beszúrásos feladattal

--- Rendezes/beszurasos.py
+++ Rendezes/beszurasos.py
@@ -5,11 +5,15 @@ def beszurasos(A):
     for j in range(1, len(A)):
         kulcs = A[j]
         i = j - 1
+        # print(A)
         while i >= 0 and A[i] > kulcs:
             A[i + 1] = A[i]
             i -= 1
-
         A[i + 1] = kulcs
-
+    # print(A)
 
 tesztelo(beszurasos, 10000)
+
+# Feladat
+# A = [8, 2, 1, 5, 6, 9, 4, 3, 7]
+# beszurasos(A)
