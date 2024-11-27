db8163f026cf0ca3772bf5f5eab2fbd5c134b364
BirkasTivadar <birkastivadar@gmail.com>
2024-11-27 21:22:16 +0100

    Kupacol feladattal

--- Rendezes/Kupacol.py
+++ Rendezes/Kupacol.py
@@ -9,13 +9,14 @@ def bal(i):
 def jobb(i):
     return (2 * i) + 1
 
+
 def maximumKupacol(A, i):
     l = bal(i)
     r = jobb(i)
     kupacmeretA = len(A)
     l -= 1
     r -= 1
-    i -=1
+    i -= 1
 
     if l < kupacmeretA and A[l] > A[i]:
         legnagyobb = l
@@ -28,6 +29,7 @@ def maximumKupacol(A, i):
     if legnagyobb != i:
         A[i], A[legnagyobb] = A[legnagyobb], A[i]
         maximumKupacol(A, legnagyobb + 1)
+
 
 A = [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]
 
